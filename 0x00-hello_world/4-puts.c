628e0b4ada66226ee30229a79e7f8853e7be257e
prosperReigns <prosperkinging7@gmail.com>
2023-06-15 12:42:46 -0700

    Print description of programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-$CFILE
+#include <stdio.h>
+
+/**
+*main - Entry point
+*Return: Always 0(success)
+*/
+
+int main(void)
+{
+    puts("Programming is like building a multilingual puzzle\n");
+    return (0);
+}
