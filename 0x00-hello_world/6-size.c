aa49d0a45ef462fcf08dcae97d41a4fc05d8add9
prosperReigns <prosperkinging7@gmail.com>
2023-06-15 13:52:55 -0700

    Memory size of various data type in c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,15 +8,10 @@
 
 int main()
 {
-char c;
-int i;
-long int a;
-long long int v;
-float f
-printf("Size of a char: %c\n", sizeof(c));
-printf("Size of an int: %d\n", sizeof(i));
-printf("Size of a long int: %li\n", sizeof(a));
-printf("Size of a long long int: %lli\n", sizeof(v));
-printf("Size of a float: %lu\n", sizeof(f));
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
