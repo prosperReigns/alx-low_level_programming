7c83d7f04ac6cae2d8278b7f1536a012cc6b2c25
prosperReigns <prosperkinging7@gmail.com>
2023-06-16 15:28:51 -0700

    Display hexadecimal values

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+*main - print hexadecimal values
+*Description: using putchar function int are converted to ascii
+*characters are printed along side int
+*Return: Always 0 (successful)
+*/
+
+int main(void)
+{
+int num, ch;
+num = 0;
+while (num < 10)
+{
+putchar(num + '0');
+num++;
+}
+ch = a;
+while (ch < 'g')
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
+}
