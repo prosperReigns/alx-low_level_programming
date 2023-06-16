6a01ec746716cef765de57f59682c4df81e3ebbc
prosperReigns <prosperkinging7@gmail.com>
2023-06-16 14:41:15 -0700

    Write 0 - 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+*main - Number 0 - 9
+*Description: Prints single digit in base10
+*Return: Always 0 (successful)
+*/
+
+int main(void)
+{
+int num = 0;
+while (num < 10)
+{
+printf("%d", num);
+num++;
+}
+return (0);
+}
