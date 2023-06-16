2b970387b7b45ca029bf1f39885955aebef483a9
prosperReigns <prosperkinging7@gmail.com>
2023-06-16 15:47:32 -0700

    Separate by comma and space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*Description: digit 0 to 9 separated by a comma and space
+*Return: Always 0 (successful)
+*/
+
+int main(void)
+{
+int num = 0;
+while (num < 10)
+{
+putchar(num + '0');
+putchar(',');
+putchar(' ');
+num++;
+}
+putchar('\n');
+return (0);
+}
