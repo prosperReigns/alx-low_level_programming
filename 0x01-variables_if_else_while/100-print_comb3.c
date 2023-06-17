3147cf7efcf63ba4cc0e5fc282963bf430b4056d
prosperReigns <prosperkinging7@gmail.com>
2023-06-17 15:12:41 -0700

    Combine two digit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+*main - combine two digits
+*Description: Two variable of type int to store two digits
+*first varible holds digit with position value tenth
+*second variable holds digit with positional value unit
+*assuming the number is 0
+*Return:  Always 0 (successful)
+ */
+
+int main(void)
+{
+	int num = 0;
+	int first, second;
+
+	while (num < 99)
+	{
+		first = num / 10;
+		second = num % 10;
+		if (first < second)
+		{
+			putchar(first + '0');
+			putchar(second + '0');
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	return (0);
+}
