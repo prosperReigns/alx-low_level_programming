f3e45dd403bd7ba37c7d718416718bae3154a7aa
prosperReigns <prosperkinging7@gmail.com>
2023-06-17 17:42:14 -0700

    Output two 2-digit number combination

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+*main - two two digit combination
+*Description: Two variables of data type integer
+*first variable holds the first two digit
+*second variable holds secont two digits
+*Return: Always o (successful)
+*/
+
+int main(void)
+{
+	int first, second;
+	for (first = 0; first < 99; first++)
+	{
+		for (second = 0; second < 99; second++)
+		{
+			putchar(first / 10 + '0');
+			putchar(first % 10 + '0');
+			putchar(' ');
+			putchar(second / 10 '0');
+			putchar(second % 0 + '0');
+			if (first != 98 || second != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
