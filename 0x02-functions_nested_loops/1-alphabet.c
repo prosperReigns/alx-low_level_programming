16c747125a3fd4173caf37ee1fd4eef78d29ae0a
prosperReigns <prosperkinging7@gmail.com>
2023-06-20 13:40:31 -0700

    print a-z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	print_alphabet(void);
+	return (0);
+}
+
+/**
+ *print_alphabet - print alphabets
+ *Description: alphabet a-z in lowercase
+ *Return: Always void
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	alpha = 'a';
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+	return (void);
+}
