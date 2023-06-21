2c8290c1197c887c16cbf769d75f8589a9b4cd94
prosperReigns <prosperkinging7@gmail.com>
2023-06-21 04:21:29 -0700

    24 hours format

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,27 +1,25 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
- *
+ *jack_bauer - 24 hours in long format
+ *Description: time for 24 hours
+ *Return:no value returned
  */
 
 void jack_bauer(void)
 {
 	int hour, minutes;
 
-	for (hour = 0; hour < 24;hour++)
+	for (hour = 0; hour < 24; hour++)
 	{
 		for (minutes = 0; minutes < 60; minutes++)
 		{
-			_putchar(hour / 10);
-			_putchar(hour % 10);
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
 			_putchar(':');
-			_putchar(minutes / 10);
-			_putchar(minutes % 10);
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
