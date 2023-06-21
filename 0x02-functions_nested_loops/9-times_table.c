8a5821f1fc3250f948e23024e35f7b3609fac899
prosperReigns <prosperkinging7@gmail.com>
2023-06-21 09:06:21 -0700

    multiplication table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ *times_table - 9x
+ *Description: prints the 9x table
+ *Return: no return value
+ */
+
+void times_table(void)
+{
+	int num, result, first, second;
+	int count = 0;
+
+	while (count < 10)
+	{
+		num = 0;
+		while (num < 10)
+		{
+			result = count * num;
+			first = result / 10;
+			second = result % 10;
+			if (first == 0)
+			{
+				_putchar(' ');
+				_putchar(second + '0');
+			}
+			else
+			{
+				_putchar(first + '0');
+				_putchar(second + '0');
+			}
+			if (num < 9)
+			{
+			_putchar(',');
+			_putchar(' ');
+			}
+			num++;
+		}
+		_putchar('\n');
+		count++;
+	}
+}
