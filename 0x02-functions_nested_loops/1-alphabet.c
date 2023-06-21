957f3077d300a53a1961ac3251e87959dcc09da3
prosperReigns <prosperkinging7@gmail.com>
2023-06-20 17:19:24 -0700

    Print alphabet a - z test2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,4 @@
 #include "main.h"
-
-/**
- *main - Entry point
- *
- *Return: Always 0
- */
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
 /**
  *print_alphabet - print alphabets
  *Description: alphabet a-z in lowercase
