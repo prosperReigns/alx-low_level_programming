b48f33eb85011aa00e92ac7020cb6bd03abf8130
prosperReigns <prosperkinging7@gmail.com>
2023-06-21 09:34:33 -0700

    Sum two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *int add(int, int) - adds two numbers
+ *@a: An integer as an arguement
+ *@b: An integer as an arguement
+ *
+ *Description: takes two values fron a user and return the sum
+ *Return: Always sum
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
