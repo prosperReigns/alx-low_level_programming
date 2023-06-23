34cc22e682668106e860d9c6c6c40a4e9d882d95
prosperReigns <prosperkinging7@gmail.com>
2023-06-23 05:00:34 -0700

    Diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ *print_diagonal - prints \ diagonally on newline
+ *@n: An integer
+ *Return: No value returned
+ */
+
+void print_diagonal(int n)
+{
+	if (n < 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			if (i == 1)
+			{
+				_putchar('\\');
+				_putchar('\n');
+			}
+			else
+			{
+				int j;
+
+				for (j = 1; j < i; j++)
+				{
+					_putchar(' ');
+				}
+				_putchar('\\');
+				_putchar('\n');
+			}
+		}
+	}
+}
