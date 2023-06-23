9868f82a3705d27d360cd3e2c05005669115266f
prosperReigns <prosperkinging7@gmail.com>
2023-06-23 05:05:13 -0700

    Print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,7 +8,7 @@
 
 void print_line(int n)
 {
-	if (n < 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
@@ -18,7 +18,7 @@ void print_line(int n)
 
 		for (i = 1; i <= n; i++)
 		{
-			_putchar('-');
+			_putchar('_');
 		}
 		_putchar('\n');
 	}
