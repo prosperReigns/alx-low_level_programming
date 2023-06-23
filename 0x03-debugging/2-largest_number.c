66d4d380b2500ba876abc59a1684d63e2856d650
prosperReigns <prosperkinging7@gmail.com>
2023-06-23 07:15:24 -0700

    Debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,7 +14,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if (a == b || b == c)
+	{
+		largest = a;
+	}
+	else if (a > b && a > c)
 	{
 		largest = a;
 	}
