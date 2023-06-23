acd453b2a7c86ebf2aa22f7dd00d7bff60919896
prosperReigns <prosperkinging7@gmail.com>
2023-06-23 05:48:07 -0700

    Fizz-Buzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ *main - Fizz-Buzz test
+ *Description: prints fizz if number is a multiple of 3
+ *prints Buzz if number is a multiple of 5
+ *print FizzBuzz if number is a multiple of 3 and 5
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+			printf(" ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+			printf(" ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+			printf(" ");
+		}
+		else
+		{
+			printf("%d", i);
+			printf(" ");
+		}
+	}
+	return (0);
+}
