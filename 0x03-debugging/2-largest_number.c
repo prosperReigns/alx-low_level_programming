e9b03a57eb6d6805123f189f1c247c22dfc12b44
prosperReigns <prosperkinging7@gmail.com>
2023-06-23 13:41:14 -0700

    Largest of three numbers

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,11 +14,7 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a == b || b == c)
-	{
-		largest = a;
-	}
-	else if (a > b && a > c)
+	if (a > b && a > c)
 	{
 		largest = a;
 	}
