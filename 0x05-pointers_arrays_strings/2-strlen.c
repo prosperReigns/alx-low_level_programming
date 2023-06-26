957878e2f456fb1644a4757eeeb9efcddfa353d0
prosperReigns <prosperkinging7@gmail.com>
2023-06-26 13:04:33 -0700

    Lenght of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *_srtlen - The lenght of a string
+ *@s: points to the address of a data value
+ *Return: Always len
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
