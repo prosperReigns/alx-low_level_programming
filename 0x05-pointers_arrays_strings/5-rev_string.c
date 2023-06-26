2ecba4c4d78b83b659866960eb5a828db78b1063
prosperReigns <prosperkinging7@gmail.com>
2023-06-26 16:36:07 -0700

    Reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *rev_string - reverse a string
+ *@s: points to the base of an array
+ *Return: nothing
+ */
+
+void rev_string(char *s)
+{
+	int i, len, temp;
+
+	len = strlen(s);
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - 1 - 1];
+		s[len - i - 1] = temp;
+	}
+}
