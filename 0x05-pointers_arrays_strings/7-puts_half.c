671168cad0d38bfffe9e9189ad721ba82c9cce06
prosperReigns <prosperkinging7@gmail.com>
2023-06-27 06:05:08 -0700

    Output last half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,7 +14,7 @@ void puts_half(char *str)
 	len = strlen(str);
 	if (len % 2 != 0)
 	{
-		for (i = (len - 1) / 2; i < len; i++)
+		for (i = (len + 1) / 2; i < len; i++)
 		{
 			_putchar(str[i]);
 		}
