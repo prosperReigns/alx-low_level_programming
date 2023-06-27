42a7fee68b0056e68626955b536113394161447b
prosperReigns <prosperkinging7@gmail.com>
2023-06-26 18:58:30 -0700

    Print even numbers

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,13 +10,15 @@
 void puts2(char *str)
 {
 	int i, len;
+	int other_ch = 0;
 
 	len = strlen(str);
 	for (i = 0; i < len; i++)
 	{
 		if (str[i] % 2 == 0)
 		{
-			_putchar(str[i]);
+			other_ch = str[i];
+			_putchar(other_ch++);
 		}
 	}
 	_putchar('\n');
