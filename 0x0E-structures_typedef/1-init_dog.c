8db629f44aedf7ca164589761e73c35249cfce20
prosperReigns <prosperkinging7@gmail.com>
2023-07-17 09:48:39 -0700

    Inintialise a struture

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ *init_dog - initialsies a object structure
+ *@d: variable
+ *@name: first member
+ *@age: second member
+ *@owner: third member
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
