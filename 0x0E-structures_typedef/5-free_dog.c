20b0ef98ed093ed26b471de591af945946e01153
prosperReigns <prosperkinging7@gmail.com>
2023-07-18 02:37:40 -0700

    free memory

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ *free_dog - frees memory allocated in heap
+ *@d: struct variable
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
