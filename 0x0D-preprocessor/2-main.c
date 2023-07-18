e804451c0844328142c4fae5333bcbb693eb4759
prosperReigns <prosperkinging7@gmail.com>
2023-07-18 05:41:30 -0700

    Print source code name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - prints the name of the source code
+ *@void: void
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	printf(__FILE__);
+	printf("\n");
+	return (0);
+}
