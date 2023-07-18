a386fd6bf98e0429f376d5d2d40d79a427d890c1
prosperReigns <prosperkinging7@gmail.com>
2023-07-18 01:32:56 -0700

    Print struct variable

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *print_dog - prints structure of dog
+ *@d: variable of struct
+ *Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		exit(EXIT_FAILURE);
+	if (d->name == NULL)
+		d->name = "nil";
+	if (d->age == 0)
+		d->age = 0;
+	if (d->owner == NULL)
+		d->owner = "nil";
+	printf("Name: %s\n Age: %f\n Owner: %s\n", d->name, d->age, d->owner);
+}
