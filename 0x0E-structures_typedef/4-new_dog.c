a88033b5cc04d71f0e5e65d5ea64aa0133aa32e7
prosperReigns <prosperkinging7@gmail.com>
2023-07-18 02:15:53 -0700

    Create anew dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,19 +2,21 @@
 #include <stdlib.h>
 
 /**
- *
- *
- *
- *
+ *new_dog - creates a new dog
+ *@name: first element
+ *@age: second element
+ *@owner: third element
+ *Return: a pointer to a memory location
  */
 
-dog_t *new_dog(char *name,__attribute__((unused)) float age, char *owner)
+dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *old_dog = malloc(sizeof(char*)+sizeof(float)+sizeof(char*));
+	dog_t *old_dog = malloc(sizeof(dog_t));
 
 	if (old_dog == NULL)
 		return (NULL);
 	old_dog->name = name;
+	old_dog->age = age;
 	old_dog->owner = owner;
 	return (old_dog);
 }
