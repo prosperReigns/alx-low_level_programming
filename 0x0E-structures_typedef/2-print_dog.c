4fa56591b5bc3e606b766e0bb7ecfadaa39e460e
prosperReigns <prosperkinging7@gmail.com>
2023-07-18 01:41:00 -0700

    Print struct variable

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,11 +11,9 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-		exit(EXIT_FAILURE);
+		return;
 	if (d->name == NULL)
 		d->name = "(nil)";
-	if (d->age == 0)
-		d->age = 0;
 	if (d->owner == NULL)
 		d->owner = "(nil)";
 	printf("Name: %s\n Age: %f\n Owner: %s\n", d->name, d->age, d->owner);
