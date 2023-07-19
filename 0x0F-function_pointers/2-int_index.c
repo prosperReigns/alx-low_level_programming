0080a8ae0dd31137e9e244b6a10863c6bbddb756
prosperReigns <prosperkinging7@gmail.com>
2023-07-19 14:14:30 -0700

    Search though an array

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,17 +1,17 @@
 #include "function_pointers.h"
 
 /**
- *
- *
- *
- *
- *
+ *int_index - searches throught an integer
+ *@array: an array
+ *@size: size of array
+ *@cmp: a pointer to a function
+ *Return: first element not equal to 0
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
-	int result = 0;
+	int result = 0, all_element = 0;
 
 	if (size <= 0)
 		return (-1);
@@ -20,11 +20,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 	while (i < size)
 	{
 		result = cmp(array[i]);
-		if (result == 0)
-			return (-1);
-		else
-			return (array[i]);
+		all_element = all_element + result;
+		if (result == 1)
+			break;
+		i++;
 	}
+	if (all_element == 0)
+		return (-1);
 	}
-	return (0);
+	return (i);
 }
