25dc88163029eef159225693065adf049ebed931
prosperReigns <prosperkinging7@gmail.com>
2024-01-26 14:20:21 -0800

    add node to list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -4,7 +4,7 @@
  *insert_dnodeint_at_index - inserts a node at a specific position
  *@h: a pointer to a pointer to
  *a doubly linked list
- *@idx: position where we you want to insert a node 
+ *@idx: position where we you want to insert a node
  *@n: the data which will be stored
  *
  *Return: a pointer to a node
@@ -12,44 +12,48 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	int count = 0;
-	dlistint_t *tmp, *newNode = NULL;
+	unsigned int i = 0;
+	dlistint_t *tmp, *tmp2, *newNode = NULL;
 
 	tmp = *h;
-	count = countNode(tmp);
-
-	if (n > count)
-		return (NULL);
-
-	newNode = createNode(n);
-	tmp = *h;
-
-	while (idx > 1)
+	while (i < idx)
 	{
+		i++;
+
 		if (tmp == NULL)
-		{
-			free(newNode);
 			return (NULL);
-		}	
 
 		tmp = tmp->next;
-		idx--;
 	}
 
-	if (tmp->prev != NULL)
-		tmp->prev->next = newNode;
-	newNode->prev = tmp->prev;
+	newNode = createNode(n);
+
+	if (tmp == NULL)
+	{
+		free(newNode);
+		return (NULL);
+	}
+
+	if (tmp->prev == NULL)
+	{
+		newNode->next = tmp;
+		tmp->prev = newNode;
+		*h = newNode;
+		return (*h);
+	}
+
+	tmp2 = tmp->prev;
+	newNode->prev = tmp2;
 	newNode->next = tmp;
 	tmp->prev = newNode;
-
-	if (idx == 1)
-		*h = newNode;
+	tmp2->next = newNode;
 
 	return (*h);
 }
 
 /**
  *createNode- ctrates a node
+ *@n: value you want to store
  *
  *Return: a pointer to a node
  */
@@ -67,24 +71,4 @@ dlistint_t *createNode(int n)
 	newNode->prev = NULL;
 
 	return (newNode);
-}
-
-/**
- *countNode - gets the number of node in a list 
- *@tmp: a pointer to a list
- *
- *Return: number of item in a list
- */
-
-int countNode(dlistint_t *tmp)
-{
-	int count = 0;
-
-	while (tmp != NULL)
-	{
-		count++;
-		tmp = tmp->next;
-	}
-
-	return (count);
 }
