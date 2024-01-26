f8e622a4b4b83777c8ca6a2526fd0089466ae564
prosperReigns <prosperkinging7@gmail.com>
2024-01-26 13:22:09 -0800

    all variations didnt work

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,37 +13,16 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	int count = 0;
-	dlistint_t *tmp;
-	dlistint_t *newNode = NULL;
+	dlistint_t *tmp, *newNode = NULL;
 
 	tmp = *h;
-
-	while (tmp != NULL)
-	{
-		count++;
-		tmp = tmp->next;
-	}
+	count = countNode(tmp);
 
 	if (n > count)
 		return (NULL);
 
-	newNode = malloc(sizeof(dlistint_t));
-	if (newNode == NULL)
-		return (NULL);
-
-	newNode->n = n;
-	newNode->next = NULL;
-	newNode->prev = NULL;
-
+	newNode = createNode(n);
 	tmp = *h;
-
-	if (idx == 0)
-	{
-		newNode->next = tmp;
-		tmp->prev = newNode;
-		*h = newNode;
-		return (newNode);
-	}
 
 	while (idx > 1)
 	{
@@ -57,15 +36,55 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		idx--;
 	}
 
-	newNode->next = tmp->next;
+	if (tmp->prev != NULL)
+		tmp->prev->next = newNode;
+	newNode->prev = tmp->prev;
+	newNode->next = tmp;
+	tmp->prev = newNode;
 
-	if (tmp->next != NULL)
-	{
-		tmp->next->prev = newNode;
-	}
+	if (idx == 1)
+		*h = newNode;
 
-	tmp->next = newNode;
-	newNode->prev = tmp;
+	return (*h);
+}
+
+/**
+ *createNode- ctrates a node
+ *
+ *Return: a pointer to a node
+ */
+
+dlistint_t *createNode(int n)
+{
+	dlistint_t *newNode = NULL;
+
+	newNode = malloc(sizeof(dlistint_t));
+	if (newNode == NULL)
+		return (NULL);
+
+	newNode->n = n;
+	newNode->next = NULL;
+	newNode->prev = NULL;
 
 	return (newNode);
+}
+
+/**
+ *countNode - gets the number of node in a list 
+ *@tmp: a pointer to a list
+ *
+ *Return: number of item in a list
+ */
+
+int countNode(dlistint_t *tmp)
+{
+	int count = 0;
+
+	while (tmp != NULL)
+	{
+		count++;
+		tmp = tmp->next;
+	}
+
+	return (count);
 }
